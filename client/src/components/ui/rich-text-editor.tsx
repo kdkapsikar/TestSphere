b980892facce508985c9258dc34ddf3473a63777
kdkapsikar <ketan.kapsikar@gmail.com>
2025-09-14 13:41:49 +0530

    Fix Table extension import error

--- client/src/components/ui/rich-text-editor.tsx
+++ client/src/components/ui/rich-text-editor.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Placeholder from '@tiptap/extension-placeholder';
-import Table from '@tiptap/extension-table';
+import { Table } from '@tiptap/extension-table';
 import TableRow from '@tiptap/extension-table-row';
 import TableHeader from '@tiptap/extension-table-header';
 import TableCell from '@tiptap/extension-table-cell';
